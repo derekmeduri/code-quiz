3590cfebecccaf2f940254b52bc4055075cfc14b
Derek Meduri <derekmeduri@gmail.com>
2023-05-22 13:24:04 -0400

    adding place holder questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -5,5 +5,25 @@ var questions = [
     choices: ['placeholder 1', 'placeholder 2', 'placeholder 3', 'placeholder 4'],
     answer: 'placeholder 1',
   },
+  {
+    title: 'Test Question 2';
+    choices: ['placeholder 1', 'place holder 2', 'place holder 3', 'place holder 4'],
+    answer: 'placeholder 1'
+  },
+  {
+    title: 'Test Question 3';
+    choices: ['placeholder 1', 'place holder 2', 'place holder 3', 'place holder 4'],
+    answer: 'placeholder 1'
+  },
+  {
+    title: 'Test Question 4';
+    choices: ['placeholder 1', 'place holder 2', 'place holder 3', 'place holder 4'],
+    answer: 'placeholder 1'
+  },
+  {
+    title: 'Test Question 5';
+    choices: ['placeholder 1', 'place holder 2', 'place holder 3', 'place holder 4'],
+    answer: 'placeholder 1'
+  },
 
 ];
