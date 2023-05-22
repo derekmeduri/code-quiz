53dc48a3d213c39461fe4cca06e58022f55f85cb
Derek Meduri <derekmeduri@gmail.com>
2023-05-22 13:26:17 -0400

    adding questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,29 +1,53 @@
 // list of all questions, choices, and answers
 var questions = [
   {
-    title: 'Test Question',
-    choices: ['placeholder 1', 'placeholder 2', 'placeholder 3', 'placeholder 4'],
-    answer: 'placeholder 1',
+    title: "Test Question",
+    choices: [
+      "placeholder 1",
+      "placeholder 2",
+      "placeholder 3",
+      "placeholder 4",
+    ],
+    answer: "placeholder 1",
   },
   {
-    title: 'Test Question 2';
-    choices: ['placeholder 1', 'place holder 2', 'place holder 3', 'place holder 4'],
-    answer: 'placeholder 1'
+    title: "Test Question 2",
+    choices: [
+      "placeholder 1",
+      "place holder 2",
+      "place holder 3",
+      "place holder 4",
+    ],
+    answer: "placeholder 1",
   },
   {
-    title: 'Test Question 3';
-    choices: ['placeholder 1', 'place holder 2', 'place holder 3', 'place holder 4'],
-    answer: 'placeholder 1'
+    title: "Test Question 3",
+    choices: [
+      "placeholder 1",
+      "place holder 2",
+      "place holder 3",
+      "place holder 4",
+    ],
+    answer: "placeholder 1",
   },
   {
-    title: 'Test Question 4';
-    choices: ['placeholder 1', 'place holder 2', 'place holder 3', 'place holder 4'],
-    answer: 'placeholder 1'
+    title: "Test Question 4",
+    choices: [
+      "placeholder 1",
+      "place holder 2",
+      "place holder 3",
+      "place holder 4",
+    ],
+    answer: "placeholder 1",
   },
   {
-    title: 'Test Question 5';
-    choices: ['placeholder 1', 'place holder 2', 'place holder 3', 'place holder 4'],
-    answer: 'placeholder 1'
+    title: "Test Question 5",
+    choices: [
+      "placeholder 1",
+      "place holder 2",
+      "place holder 3",
+      "place holder 4",
+    ],
+    answer: "placeholder 1",
   },
-
 ];
